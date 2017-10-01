aeb28cb63275d46eae6db47968fd3984349e311a
Petr Řehoř <rx@rx.cz>
2017-10-02 01:14:06 +0200

    Fix typo

--- docker.image.mk
+++ docker.image.mk
@@ -956,8 +956,8 @@ docker-push:
 	@$(foreach TAG,$(DOCKER_PUSH_TAGS),docker push $(DOCKER_REGISTRY)/$(DOCKER_IMAGE_NAME):$(TAG);echo;)
 
 # Load the base image from file
-.PHONY: docker-load-base-image
-docker-load-base-image:
+.PHONY: docker-load-baseimage
+docker-load-baseimage:
 	cat $(DOCKER_IMAGE_DEPOT)/$(shell echo $(BASE_IMAGE) | sed -E "s/[^[:alnum:].]/-/g").image | \
 	gunzip | docker image load
 
